db0d9d667c36871d69adfb3f2d718c33e1c816f3
sauravkrkushwaha <sauravkumar27121001@gmail.com>
2023-06-12 19:14:33 +0530

    Initial Commit

---
+++ LeetCode_2/Main.java
@@ -0,0 +1,68 @@
+package LeetCode_2;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            carry = sum / 10;
+        }
+
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return dummy.next;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create the input linked lists
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        // Create an instance of the Solution class
+        Solution solution = new Solution();
+
+        // Call the addTwoNumbers method
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        // Print the result
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
